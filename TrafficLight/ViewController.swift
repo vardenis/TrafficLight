0ced9fc0fda803cb9224300d350d0a77a31ab0b0
Denis <varenikovda@gmail.com>
2022-01-27 17:46:11 +0300

    Update ViewController

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,11 +9,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var orangeLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    
+    @IBOutlet var buttonActionTitle: UIButton!
+    var lightSwitch = "start"
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLight.layer.cornerRadius = 73
+        redLight.alpha = 0.3
+        
+        orangeLight.layer.cornerRadius = 73
+        orangeLight.alpha = 0.3
+        
+        greenLight.layer.cornerRadius = 73
+        greenLight.alpha = 0.3
+        
+        buttonActionTitle.setTitle("START", for: .normal)
     }
 
-
+    @IBAction func colorSwitching() {
+        switch lightSwitch {
+        case _ where lightSwitch == "red":
+            lightSwitch = "orange"
+            redLight.alpha = 0.3
+            orangeLight.alpha = 1
+        case _ where lightSwitch == "orange":
+            lightSwitch = "green"
+            orangeLight.alpha = 0.3
+            greenLight.alpha = 1
+        case _ where lightSwitch == "green":
+            lightSwitch = "red"
+            greenLight.alpha = 0.3
+            redLight.alpha = 1
+        default:
+            lightSwitch = "red"
+            redLight.alpha = 1
+            buttonActionTitle.setTitle("NEXT", for: .normal)
+        }
+        
+    }
+    
 }
 
